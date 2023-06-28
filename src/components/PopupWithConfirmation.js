1a931a4fa13c54d848f34c4a0a2f9aeccb35a765
Ivan Akimov <dartvayder582@yandex.ru>
2023-06-28 13:54:26 +0300

    проверить функциональность проекта

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -3,7 +3,7 @@ import Popup from './Popup.js';
 export default class PopupWithConfirmation extends Popup {
   constructor(popupSelector, { confirmFunction }) {
     super(popupSelector);
-    this._confirmButton = this._popup.querySelector('.confirm__button');
+    this._confirmButton = this._popup.querySelector('.form__submit-button');
     this._confirmFunction = confirmFunction;
   }
 
